75e602ab7ae915ce5d515f3353067f4941b4b6d1
BRO-BHATT <525sunaybhatt@frhsd.com>
2024-02-23 11:23:39 -0500

    Finished the Javascript for writing your name and inputting it in the game.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-let name = prompt('What is your name?')
+window.onload = (event) => {
+    let userName = prompt('What is your name?')
+
+    if(userName === null) {
+        document.getElementById('response-text').innerText = "Yo enter a name yo are not sneaky"
+        document.getElementById('response-text').classList = "Display text-bold h3"
+    }
+
+    if(!userName.trim()) {
+        alert('please enter a name brother')
+        userName = prompt('Eneter your name')
+    }
+document.getElementById('response-text').innerText = `${userName}`
+
+}
+
+let message = `Yo good job ${userName}, you won!`
+
